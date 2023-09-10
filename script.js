d953bfde5499141271d65c4620f0381a743ec1cb
PALLE RAVINDRA KUMAR <119121995+ravindrakumar05@users.noreply.github.com>
2023-09-10 20:57:50 +0530

    required files are pushed

---
+++ script.js
@@ -0,0 +1,32 @@
+let inputEle = document.getElementById("location-input")
+let tempEle = document.getElementById("temp-value")
+let locEle = document.getElementById("weather-desc")
+let weatherdescEle = document.getElementById("weather-desc")
+let btnEle = document.getElementById("btn")
+let icon = document.getElementById("icon")
+
+
+const apikey = '33baafaad8283d5c121ce406c0676eba';
+
+btnEle.onclick = function(){
+    if(inputEle.value=="")
+        alert("Please Enter some location")
+    else{
+        loc = inputEle.value
+        url = `https://api.openweathermap.org/data/2.5/weather?q=${loc}&appid=${apikey}`
+        fetch(url).then(res => res.json())
+        .then(data =>{
+            console.log(data)
+            const{name} = data 
+            const{feels_like} = data.main
+            const{description} = data.weather[0]
+            tempEle.innerText = Math.floor(feels_like-273);
+            locEle.innerText = name;
+            weatherdescEle.innerText = description;
+        })
+        .catch( () =>{
+            alert("Enter valid location")
+        })
+        inputEle.value = ""
+    }
+}
